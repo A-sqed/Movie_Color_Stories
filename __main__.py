346667bbb2acb68e12e5c48716c3ddc96d0c0fbf
A-sqed <adrian.adduci@gmail.com>
2022-01-13 12:47:48 -0500

    Working OpenCV

--- __main__.py
+++ __main__.py
@@ -3,12 +3,16 @@
 # Contact: faa2160@columbia.edu
 ################################################################################
 
-import cv2 as cv
+import cv2 
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 import logging
+from PIL import Image
+import PIL
+import tqdm
 import pathlib
+from io import BytesIO
 path = pathlib.Path(__file__).parent.absolute()
 
 logger = logging.getLogger(__name__)
@@ -28,30 +32,6 @@ ch.setFormatter(formatter)
 logger.addHandler(fh)
 logger.addHandler(ch)
 
-path = ("B:\\_projects\\_the_little_mermaid.avi")
-print("Video Path: ", path)
-print(cv.__version__)
-
-#print(cv.getBuildInformation())
-vidcap = cv.VideoCapture(path)
-vidcap.cv.CV_FOURCC(*'XVID')
-success, image = vidcap.read()
-count = 0
-
-if (vidcap.isOpened()== False):
-  print("Error opening video stream or file")
-  
-while success:
-    #cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file      
-    success, image = vidcap.read()
-    # if frame is read correctly ret is True
-    if not success:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    print('Read a new frame: ', success)
-    count += 1
-print(count)
-
 def find_histogram(clt):
     """
     create a histogram with k clusters
@@ -67,31 +47,64 @@ def find_histogram(clt):
     return hist
 
 def plot_colors2(hist, centroids):
-    bar = np.zeros((50, 300, 3), dtype="uint8")
+    
+    # Size the Bar 
+    bar = np.zeros((5, 500, 3), dtype="uint8")
     startX = 0
 
     for (percent, color) in zip(hist, centroids):
         # plot the relative percentage of each cluster
         endX = startX + (percent * 300)
+
         cv2.rectangle(bar, (int(startX), 0), (int(endX), 50),
                       color.astype("uint8").tolist(), -1)
+        
         startX = endX
 
     # return the bar chart
     return bar
 
-"""
-img = cv2.imread("pic/img7.jpeg")
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#path = ("B:\\_projects\\_the_little_mermaid.")
+path = "B:\\Videos\\_BLACK_13.mp4"
+print("Video Path: ", path)
 
-img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-clt = KMeans(n_clusters=3) #cluster number
-clt.fit(img)
+cap = cv2.VideoCapture(path)
 
-hist = find_histogram(clt)
-bar = plot_colors2(hist, clt.cluster_centers_)
+if (cap.isOpened()== False):
+  print("Error opening video stream or file")
+ 
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-plt.axis("off")
-plt.imshow(bar)
-plt.show()
-"""
+print(f"Total Frame Count: {frame_count}")
+#input("Press Enter to continue...")
+
+for f in range(41000,41001):
+    ret, frame = cap.read()    
+
+    logger.info(f"Converting Frame {f}")
+    _, JPEG = cv2.imencode('.jpeg', frame)
+    img = cv2.cvtColor(JPEG, cv2.COLOR_BGR2RGB)
+
+    logger.info(f"Reshaping Frame {f}")
+    #represent as row*column,channel number
+    img = img.reshape((img.shape[0] * img.shape[1],3)) 
+
+    #cluster number
+    n_clusters = 4
+    logger.info(f"KMeans, Clusters: {n_clusters}, Frame: {f}")
+    
+    # Fit the model using the image 
+    clt = KMeans(n_clusters=n_clusters) 
+    clt.fit(img)
+
+    hist = find_histogram(clt)
+    bar = plot_colors2(hist, clt.cluster_centers_)
+    bar = bar.rotate(90, PIL.Image.NEAREST, expand = 1)
+    plt.axis("off")
+    plt.imshow(bar)
+    plt.show()
+
+
+cap.release()
+cv2.destroyAllWindows()
+
